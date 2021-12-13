fcc72810ce054acb8c169e07e0fb980033363c0e
Shuvo-BS23 <ashik.ahmmed@brainstation-23.com>
2021-12-13 15:43:32 +0600

    nav

--- src/components/TheHome.vue
+++ src/components/TheHome.vue
@@ -1,11 +1,15 @@
 <template>
+    <nav class="w-6xl">
+        <TheNavbar/>
+    </nav>
     <h1 class="text-center">hello world</h1>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-
+import TheNavbar from '@/components/TheNavbar.vue'
 export default defineComponent({
+    components:{TheNavbar},
     setup() {
         console.log('hello')
     },
---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="w-full h-20 bg-gray-600 text-white flex justify-between items-center p-5">
+        <div id="brand" class="text-white">ALGO VIZ</div>
+        <div id="menu" class="flex justify-items-center p-4 space-x-4">
+            <button class="rounded-md hover:opacity-1 hover:cursor-pointer hover:bg-white-300">Bubble Sort</button>    
+            <button class="rounded-md hover:opacity-1 hover:cursor-pointer hover:bg-white-300">marge Sort</button>
+            <button class="rounded-md hover:opacity-1 hover:cursor-pointer hover:bg-white-300">....</button>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+       console.log('nav') 
+    },
+})
+</script>
