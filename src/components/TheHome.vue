6e20dad0282fa0d23016f8f2a1d19ef8e6aa55ed
Shuvo-BS23 <ashik.ahmmed@brainstation-23.com>
2021-12-13 15:23:42 +0600

    init

---
+++ src/components/TheHome.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1 class="text-center">hello world</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+        console.log('hello')
+    },
+})
+</script>
